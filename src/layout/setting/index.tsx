955a188a44d16e38624992ee781491ca883562f8
asyncfinkd <shamgoingzuck@gmail.com>
2022-01-02 00:09:40 +0400

    Tooltip and Render by state

--- src/layout/setting/index.tsx
+++ src/layout/setting/index.tsx
@@ -2,55 +2,64 @@ import React from 'react'
 import Box from '@mui/material/Box'
 import SettingsIcon from '@mui/icons-material/Settings'
 import CloseIcon from '@mui/icons-material/Close'
+import Tooltip from '@mui/material/Tooltip'
 
 export default function Setting() {
   const [show, setShow] = React.useState<boolean>(false)
 
   return (
     <>
-      <Box
-        position="fixed"
-        display={show ? 'block' : 'none'}
-        top="0"
-        left="0"
-        bgcolor="rgba(0,0,0,0.7)"
-        width="100%"
-        height="100vh"
-        onClick={() => setShow(!show)}
-        zIndex="9"
-        sx={{
-          transition: 'background 0.3s ease 0s',
-        }}
-      ></Box>
-      <Box
-        position="fixed"
-        display={show ? 'block' : 'none'}
-        top="0"
-        right="0"
-        width="45%"
-        height="100vh"
-        bgcolor="#fff"
-        zIndex="12"
-      >
-        <Box
-          display="flex"
-          alignItems="center"
-          minHeight="88px"
-          maxHeight="88px"
-          padding="0 40px"
-          borderBottom="1px solid rgba(0, 0, 0, 0.08)"
-        >
-          <Box flexGrow="1" display="flex" alignItems="center">
-            Timer Setting
+      {show && (
+        <>
+          <Box
+            position="fixed"
+            top="0"
+            left="0"
+            bgcolor="rgba(0,0,0,0.7)"
+            width="100%"
+            height="100vh"
+            onClick={() => setShow(!show)}
+            zIndex="9"
+            sx={{
+              transition: 'background 0.3s ease 0s',
+            }}
+          ></Box>
+          <Box
+            position="fixed"
+            top="0"
+            right="0"
+            width="45%"
+            height="100vh"
+            bgcolor="#fff"
+            zIndex="12"
+          >
+            <Box
+              display="flex"
+              alignItems="center"
+              minHeight="88px"
+              maxHeight="88px"
+              padding="0 40px"
+              borderBottom="1px solid rgba(0, 0, 0, 0.08)"
+            >
+              <Box flexGrow="1" display="flex" alignItems="center">
+                Timer Setting
+              </Box>
+              <Tooltip title="Close" placement="top">
+                <Box marginLeft="16px" marginRight="-8px">
+                  <CloseIcon
+                    sx={{
+                      color: 'rgba(0,0,0,0.32)',
+                      height: 32,
+                      cursor: 'pointer',
+                    }}
+                    onClick={() => setShow(!show)}
+                  />
+                </Box>
+              </Tooltip>
+            </Box>
           </Box>
-          <Box marginLeft="16px" marginRight="-8px">
-            <CloseIcon
-              sx={{ color: 'rgba(0,0,0,0.32)', height: 32, cursor: 'pointer' }}
-              onClick={() => setShow(!show)}
-            />
-          </Box>
-        </Box>
-      </Box>
+        </>
+      )}
       <Box
         position="fixed"
         bottom="20px"
